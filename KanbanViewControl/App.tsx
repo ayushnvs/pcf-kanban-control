f677acbe6af99820e1fb68eaa0178e0e42c23a72
ayushnvs <ayush.mishra.ald@gmail.com>
2025-04-16 10:08:13 +0530

    Refactor

--- KanbanViewControl/App.tsx
+++ KanbanViewControl/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { IInputs } from './generated/ManifestTypes';
 import { Board } from './components';
-import { useMemo, useState, useEffect } from 'react';
+import { useMemo, useState } from 'react';
 import { BoardContext } from './context/board-context';
 import { ColumnItem, ViewItem, ViewEntity } from './interfaces';
 import Loading from './components/container/loading';
@@ -81,13 +81,6 @@ const App = ({ context, notificationPosition } : IProps) => {
     setSelectedEntity(dataset.getTargetEntityType())
     handleColumnsChange()
   }, [context.parameters.dataset.columns])
-
-  // useEffect to trigger handleViewChange after activeView is set initially
-  // useEffect(() => {
-  //   if (activeView) {
-  //     handleViewChange();
-  //   }
-  // }, [activeView]);
 
   const filterRecords = (activeView: ViewItem) => {
     return Object.entries(dataset.records).map(([id, record]) => {
